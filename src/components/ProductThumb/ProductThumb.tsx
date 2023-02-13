1d65d6af00605d5a50c9ed9a1b9e629344d06ada
Alex <skyriver.f1@gmail.com>
2023-02-13 19:03:04 +0500

    cart style

--- src/components/ProductThumb/ProductThumb.tsx
+++ src/components/ProductThumb/ProductThumb.tsx
@@ -10,8 +10,8 @@ interface propsProductThumb {
   getModalPopupData: Function;
   // addProductToCart: Function;
   // cart: Function;
-  handleAddProductToCart: any
-  handleRemoveFromCart: any
+  addProductToCart: any
+  removeFromCart: any
   haveInCart: any
 }
 
@@ -20,8 +20,8 @@ function ProductThumb({
   modalPopup,
   getModalPopupData,
   // addProductToCart,
-  handleAddProductToCart,
-  handleRemoveFromCart,
+  addProductToCart,
+  removeFromCart,
   haveInCart
   // cart
   }: propsProductThumb) {
@@ -71,7 +71,7 @@ function ProductThumb({
           {!haveInCart ? (
             <button className="product-thumb__button-add-to-cart ml-auto mr-3 p-1.5 border border-orange-400" type="button"
               /* onClick={() => addProductToCart(setToCart) } */
-              onClick={() => handleAddProductToCart(productData.id)}
+              onClick={() => addProductToCart(productData.id)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-orange-400">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -80,7 +80,7 @@ function ProductThumb({
           ) : (
               <button className="product-thumb__button-add-to-cart ml-auto mr-3 p-1.5 border border-orange-400 bg-orange-400" type="button"
                 /* onClick={() => addProductToCart(setToCart) } */
-                onClick={() => handleRemoveFromCart(productData.id)}
+                onClick={() => removeFromCart(productData.id)}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-white">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -59,14 +59,18 @@ function ProductsList() {
     
   // }
 
-  const handleAddProductToCart = (productID: any) => {
+  const addProductToCart = (productID: any) => {
     setCartProducts([...cartProducts, productID]);
   };
 
-  const handleRemoveFromCart = (productID: any) => {
+  const removeFromCart = (productID: any) => {
     const newCartProducts = cartProducts.filter((id: any) => id !== productID);
     setCartProducts(newCartProducts);
   };
+  // const removeFromCart = (productID: any) => {
+  //   const newCartProducts = cartProducts.filter((id: any) => id !== productID);
+  //   setCartProducts(newCartProducts);
+  // };
 
   async function getProductsData() {
     const response = await fetch(urlProducts);
@@ -103,8 +107,8 @@ function ProductsList() {
                     getModalPopupData={getModalPopupData} 
                     // addProductToCart={addProductToCart}
                     // cart={cart}
-                    handleAddProductToCart={handleAddProductToCart}
-                    handleRemoveFromCart={handleRemoveFromCart}
+                    addProductToCart={addProductToCart}
+                    removeFromCart={removeFromCart}
                     haveInCart={haveInCart}
                   />
                 </div>
@@ -131,8 +135,8 @@ function ProductsList() {
           })
         } */}
 
-        <div>
-          <h1>Корзина</h1>
+        <Title text={"Корзина"} />
+        <div className="cart-products">
           {cartProducts.length > 0
             ? cartProducts.map((productID: any) => {
               const productIndex = productsData.findIndex(product => {
@@ -140,14 +144,33 @@ function ProductsList() {
               });
               let { title, id, price, thumbnail } = productsData[productIndex];
               return (
-                <div key={id}>
-                  <p><img src={thumbnail} alt="" /></p>
-                  <p>{title}</p>
-                  <p>{price}</p>
+                <div className="cart-products__item flex border border-gray-300 p-2" key={id}>
+                  <div className="cart-products__img w-12 h-12 mr-3">
+                    <img className="w-full h-full object-cover object-center" src={thumbnail} alt="" />
+                    </div>
+                  <div className="cart-products__info flex items-center">
+                    <div className="cart-products__content mr-3">
+                      <p className="text-orange-400">{title}</p>
+                      <p><span>$</span>{price}</p>
+                    </div>
+                    <button className="cart-products__button-remove ml-auto mr-3 p-1.5 border border-red-500" type="button" onClick={() => removeFromCart(id)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-red-500">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    </button>
+                  </div>
+                  <>
+                    {console.log(cartProducts.length)}
+                  </>
                 </div>
               );
             })
-            : "Корзина пуста! :("}
+            : 
+              <div>
+                <h3 className=" text-xl">Корзина пуста!</h3>
+                <a className=" text-orange-400 underline" href="/">Вернуться на главную</a>
+              </div>
+            }
         </div>
       </>
     </div>
